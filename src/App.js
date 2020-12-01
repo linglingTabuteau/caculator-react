3b475cf877a09bcd7c199f98a70b2ebc095e2fe4
Lingling Tabuteau <linging.tabuteau@gmail.com>
2020-12-01 20:42:57 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [nbState, setnbState] = useState("");
+  const [totalState, setTotalState] = useState(0);
+
+  const pickNbHandler = (nb) => {
+    let textTotal = nbState.toString().concat(nb);
+    console.log(textTotal);
+    setnbState(textTotal);
+  };
+
+  const caculateHandler = () => {
+    console.log(eval(nbState));
+    eval(nbState);
+    setTotalState(eval(nbState));
+  };
+
+  const resetHandler = () => {
+    setnbState("");
+    setTotalState(0);
+  };
+
+  const typeHandler = () => {
+    setnbState(-nbState);
+  };
+
+  let result = <div className="Orange">result</div>;
+  if (nbState) {
+    result = nbState;
+  }
+  if (totalState) {
+    result = totalState;
+  }
+
+  let classes = ["Row"];
+  if (nbState.length > 5) {
+    classes.push("Red");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={classes.join(" ")}> {result}</div>
+      <div className="Row">
+        <div className="Orange" onClick={() => pickNbHandler(1)}>
+          1
+        </div>
+        <div className="Orange" onClick={() => pickNbHandler(2)}>
+          2
+        </div>
+        <div className="Orange" onClick={() => pickNbHandler(3)}>
+          3
+        </div>
+        <div className="Orange" onClick={() => pickNbHandler(4)}>
+          4
+        </div>
+      </div>
+      <div className="Row">
+        <div className="Orange" onClick={() => pickNbHandler(5)}>
+          5
+        </div>
+        <div className="Orange" onClick={() => pickNbHandler(6)}>
+          6
+        </div>
+        <div className="Orange" onClick={() => pickNbHandler(7)}>
+          7
+        </div>
+        <div className="Orange" onClick={() => pickNbHandler(8)}>
+          8
+        </div>
+      </div>
+      <div className="Row">
+        <div className="Orange" onClick={() => pickNbHandler(9)}>
+          9
+        </div>
+        <div className="Orange" onClick={() => pickNbHandler(0)}>
+          0
+        </div>
+        <div className="Orange" onClick={() => resetHandler()}>
+          AC
+        </div>
+        <div className="Orange" onClick={() => pickNbHandler("+")}>
+          +
+        </div>
+      </div>
+      <div className="Row">
+        <div className="Orange" onClick={() => caculateHandler("=")}>
+          =
+        </div>
+        <div className="Orange" onClick={() => pickNbHandler("-")}>
+          -
+        </div>
+        <div className="Orange" onClick={() => typeHandler("-")}>
+          +/-
+        </div>
+        <div className="Orange" onClick={() => pickNbHandler("/")}>
+          /
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
